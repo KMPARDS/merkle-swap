f2f3d8242c89ff7c331725749545a5c4e1773090
zemse <sohamzemse143@gmail.com>
2020-09-30 03:53:47 +0530

    Use util from sdk

--- src/containers/LoadWallet/UsingMetamask.tsx
+++ src/containers/LoadWallet/UsingMetamask.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Card, Button, Container } from 'react-bootstrap';
 import { ethers } from 'ethers';
-import { routine } from '../../utils';
+import { routine } from 'eraswap-sdk/dist/utils';
 
 type State = {
   displayScreen: number;
