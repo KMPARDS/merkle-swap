926b8ecd3dd5454d47a3f1f56a6374627dca685e
zemse <sohamzemse143@gmail.com>
2020-09-30 03:54:13 +0530

    Display balance and alert

--- src/containers/BunchSubmission/ModalBody.tsx
+++ src/containers/BunchSubmission/ModalBody.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { BigNumber } from 'ethers';
+import { parseEther } from 'ethers/lib/utils';
+import { lessDecimals } from '../../utils';
+import { Alert } from 'react-bootstrap';
+
+const requiredBalance = parseEther('0.05');
 
 export function ModalBody(props: { setShowModal: (newState: boolean) => any }) {
   const [ethBalance, setEthBalance] = useState<BigNumber | null>(null);
@@ -18,12 +23,21 @@ export function ModalBody(props: { setShowModal: (newState: boolean) => any }) {
       <div className="exchange-container mt20 mb20">
         <h3 className="main-prf">Bunch Submission</h3>
         <p className="prf-md-txt">
-          This step would cost you around 0.05 ETH (depending on gwei fee). You currently have
+          This step would cost you around 0.05 ETH (depending on gwei fee).
+          {ethBalance !== null ? <> You currently have {lessDecimals(ethBalance)} ETH</> : null}
         </p>
         <div className="exc-btn-box">
-          <button className="tr-pn-btn" onClick={props.setShowModal.bind(null, false)}>
-            SUBMIT
-          </button>
+          {ethBalance !== null ? (
+            ethBalance.gte(requiredBalance) ? (
+              <button className="tr-pn-btn" onClick={props.setShowModal.bind(null, false)}>
+                SUBMIT
+              </button>
+            ) : (
+              <Alert variant="danger">Please acquire 0.05 ETH balance before continuing</Alert>
+            )
+          ) : (
+            'Please wait...'
+          )}
         </div>
       </div>
     </div>
--- src/utils.ts
+++ src/utils.ts
@@ -1,58 +1,15 @@
-import { ethers } from 'ethers';
+import { BigNumber } from 'ethers';
+import { formatEther } from 'ethers/lib/utils';
 
-export function routine(fn: Function, msec: number): NodeJS.Timeout {
-  let working = true;
-  const result = fn();
-  if (result instanceof Promise) {
-    result.then(() => (working = false));
-  } else {
-    working = false;
+export function lessDecimals(input: string | BigNumber, decimals: number = 2) {
+  if (input instanceof BigNumber) {
+    input = formatEther(input);
   }
 
-  const intervalId = setInterval(async () => {
-    if (!working) {
-      working = true;
-      await fn();
-      working = false;
-    }
-  }, msec);
+  const seperated = input.split('.');
+  if (seperated[1].length > decimals) {
+    seperated[1] = seperated[1].slice(0, decimals);
+  }
 
-  return intervalId;
-}
-
-export const EraswapInfo = {
-  baseUrl: 'https://eraswap.info',
-  getAddressHref: function (address: string) {
-    return `${this.baseUrl}/address/${address}`;
-  },
-  getTxHref: function (txHash: string) {
-    return `${this.baseUrl}/txn/${txHash}`;
-  },
-};
-
-export function renderSecondsRemaining(numberOfSeconds: number): string {
-  const days = Math.floor(numberOfSeconds / 60 / 60 / 24);
-  const hours = Math.floor((numberOfSeconds - days * 60 * 60 * 24) / 60 / 60);
-  const minutes = Math.floor((numberOfSeconds - days * 60 * 60 * 24 - hours * 60 * 60) / 60);
-  const seconds = numberOfSeconds - days * 60 * 60 * 24 - hours * 60 * 60 - minutes * 60;
-
-  return `${days !== 0 ? `${days} days, ` : ''}${hours !== 0 ? `${hours} hours, ` : ''}${
-    minutes !== 0 ? `${minutes} minutes and ` : ''
-  }${seconds} seconds`;
-}
-
-export function renderTimestampRemaining(unixTimestampSeconds: number): string {
-  const currentTimestamp = Math.round(Date.now() / 1000);
-  let secondsRemaining = currentTimestamp - unixTimestampSeconds;
-  if (secondsRemaining < 0) secondsRemaining = 0;
-  return renderSecondsRemaining(secondsRemaining);
-}
-
-export function renderEthersJsError(error: any): string {
-  return (
-    (error?.error?.reason && `Error from Smart Contract ${error?.error?.reason}`) ||
-    (error?.error?.message && `Error from Blockchain: ${error?.error?.message}`) ||
-    (error?.message && `Error on UI: ${error?.message}`) ||
-    `Weird error: ${typeof error === 'object' ? JSON.stringify(error) : error}`
-  );
+  return seperated.join('.');
 }
