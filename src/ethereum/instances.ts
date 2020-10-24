088d6598f746b075f9e5fe7e3570d0048fec89ee
zemse <sohamzemse143@gmail.com>
2020-10-24 12:44:56 +0530

    Update addresses

--- src/ethereum/instances.ts
+++ src/ethereum/instances.ts
@@ -17,9 +17,9 @@ window.providerESN = new CustomProvider(
 );
 
 // Temporary wallet
-if (process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY) {
-  window.wallet = new ethers.Wallet(process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY);
-}
+// if (process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY) {
+//   window.wallet = new ethers.Wallet(process.env.REACT_APP_TEST_WALLET_PRIVATE_KEY);
+// }
 
 const env = process.env.REACT_APP_ENV ?? 'development';
 
@@ -29,9 +29,9 @@ const address = {
   // fundsManagerETH: addresses[env].ETH.fundsManager,
   // reversePlasmaESN: addresses[env].ESN.reversePlasma,
   // fundsManagerESN: addresses[env].ESN.fundsManager,
-  esContractETH: '0x4Eb258e8B091c22a19B718ee7af9922Ae54D0445',
-  plasmaManagerETH: '0xeE1e67B07FBb52bd74D261cfddbD58FD5Ea53115',
-  fundsManagerETH: '0xb8C60Eb9DB2c6DcC9C8F086A6160D2FFF62fB514',
+  esContractETH: '0x237027559f6C07A20EBa97C837b60b9815840a42',
+  plasmaManagerETH: '0xaaF33029B457A773C14DFdab4eDc4039E80fC5BF',
+  fundsManagerETH: '0x10A23bbfAfc7cc3b94fa268D965F8B78543eCFE0',
   reversePlasmaESN: '0x3bEb087e33eC0B830325991A32E3F8bb16A51317',
   fundsManagerESN: '0xc4cfb05119Ea1F59fb5a8F949288801491D00110',
 };
