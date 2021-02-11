e1603b604b93902e7d797c3300ddb5ee4e642cc4
maheshrapelli141 <maheshrapelli141@gmail.com>
2021-02-11 18:13:50 +0530

    delay set to 1000 blocks at fetch blocks

--- src/plasma-utils/fetchBlocks.ts
+++ src/plasma-utils/fetchBlocks.ts
@@ -21,7 +21,11 @@ export async function fetchBlocks(
 
   const promises = [];
   for (let i = 0; i < blockNumbersToScan.length; i++) {
-    if (bunchDepth > 14 && i % 100 === 0) delay(1000);
+    if (bunchDepth > 14 && i % 1000 === 0){
+      console.log('waiting');
+      await delay(1000);
+      console.log('resumed');
+    }
 
     const currentBlockNumber = blockNumbersToScan[i];
     const tmpPromise = new Promise(async function (resolve, reject) {
