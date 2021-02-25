c052ed0a6c7c9d24b83ed1e6135670afd604310c
maheshrapelli141 <maheshrapelli141@gmail.com>
2021-02-25 18:20:28 +0530

    fetch blocks loop make async with limit

--- src/plasma-utils/fetchBlocks.ts
+++ src/plasma-utils/fetchBlocks.ts
@@ -21,30 +21,46 @@ export async function fetchBlocks(
   const blockNumbersToScan = [...Array(2 ** bunchDepth).keys()].map((n) => n + startBlockNumber);
   const blockArray: BlockCompact[] = new Array(2 ** bunchDepth);
 
+  let promiseArray = [];
   for (let i = 0; i < blockNumbersToScan.length; i++) {
     const currentBlockNumber = blockNumbersToScan[i];
 
-    if (bunchDepth > 14 && i % 200 === 0) {
-      // console.log(`waiting at ${currentBlockNumber}`);
-      // await delay(1500);
-      // console.log('resumed');
-      console.log(`${(i/blockNumbersToScan.length)*100}% completed`);
-      provider = switchProvider(provider);
-      console.log(`provider switched to ${provider.connection.url} at block ${currentBlockNumber}`);
+    //resolve every 4000 promises
+    if(i%1500 === 0){
+      console.log(`resolving last ${promiseArray.length} calls`);
+      await Promise.all(promiseArray);
+      promiseArray = [];
+      console.log('calls resolved, continuing furthur block calls');
     }
+    const promise = (async () => {
+      if (bunchDepth > 14) {
+        if(i % 200 === 0){
+          console.log(`${(i/blockNumbersToScan.length)*100}% completed`);
+          provider = switchProvider(provider);
+          console.log(`provider switched to ${provider.connection.url} at block ${currentBlockNumber}`);
+        }
+        // if(i % 1000 === 0){
+        //   console.log(`delaying 2 secs at block ${currentBlockNumber}`);
+        //   await delay(2000);
+        //   console.log(`resumed of block ${currentBlockNumber}`);
+        // }
+      }
+  
+      const blockNumberHex = ethers.utils.hexStripZeros(ethers.utils.hexlify(currentBlockNumber));
+  
+      const block: ParityBlock = await provider.send('eth_getBlockByNumber', [
+        blockNumberHex,
+        true,
+      ]);
+  
+      blockArray[currentBlockNumber - startBlockNumber] = {
+        blockNumber: currentBlockNumber,
+        transactionsRoot: block.transactionsRoot,
+        receiptsRoot: block.receiptsRoot,
+      };
+    })();
 
-    const blockNumberHex = ethers.utils.hexStripZeros(ethers.utils.hexlify(currentBlockNumber));
-
-    const block: ParityBlock = await provider.send('eth_getBlockByNumber', [
-      blockNumberHex,
-      true,
-    ]);
-
-    blockArray[currentBlockNumber - startBlockNumber] = {
-      blockNumber: currentBlockNumber,
-      transactionsRoot: block.transactionsRoot,
-      receiptsRoot: block.receiptsRoot,
-    };
+    promiseArray.push(promise);  
   }
 
   return blockArray;
